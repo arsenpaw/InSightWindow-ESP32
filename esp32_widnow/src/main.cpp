1bbf09c33b669748890df01df76d4308d9cd0842
Arsen Pavliuk <arsenpaw7@gmail.com>
2024-03-20 19:28:59 +0200

    Add sendind data to endpoint

--- esp32_widnow/src/main.cpp
+++ esp32_widnow/src/main.cpp
@@ -4,6 +4,13 @@
 #include <ESP32Servo.h>
 #include <Wire.h>
 #include <WiFi.h>
+#include <string.h>
+#include <HTTPClient.h>
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <ArduinoJson.h>
+
 WiFiServer server(80);
 #define magnet_switch 19  
 #define alarm_sound 26
@@ -13,6 +20,7 @@ WiFiServer server(80);
 #define DHTPIN 33  
 #define DHTTYPE DHT11  
 DHT dht(DHTPIN, DHTTYPE);
+std::string str;
 Servo servo1;
 int rain;
 int h;
@@ -21,14 +29,15 @@ boolean button1;
 boolean but_flag;
 String header;
 boolean is_protected = false;
+char jsonOutput[128];
 const char* ssid     = "ESP32-Network";
 const char* password = "11111111";
 unsigned long currentTime = millis();
-// Previous time
 unsigned long previousTime = 0;
-// Define timeout time in milliseconds
 const long timeoutTime = 2000;
 void setup()
+
+
 {
   Serial.begin(115200);
   pinMode(alarm_sound, OUTPUT);
@@ -38,12 +47,14 @@ void setup()
   pinMode(button, INPUT_PULLUP);
   pinMode(magnet_switch, INPUT_PULLUP);
   dht.begin();
-   WiFi.softAP(ssid,password);
-    Serial.println("");
+  WiFi.softAP(ssid,password);
+  Serial.println("Connecting");
   Serial.println("IP address: ");
   Serial.println(WiFi.softAPIP());
   server.begin();
+  
 }
+
   
 boolean is_window_open()
 {
@@ -97,18 +108,22 @@ void short_warning()
   noTone(buzzer);
 }
 
-void win_close()
+boolean win_close()
 {
   Serial.print("FUNC CLOSE");
   Serial.println(" ");
   servo1.attach(servo_pin);
   servo1.write(110);
-  delay(1200);
+  delay(1000);
   servo1.detach();
   if (is_window_open() == 1)
     {
-      warning();
+      return false;
     }
+  else
+  {
+      return true;
+  }
 }
 
 void win_open()
@@ -117,9 +132,8 @@ void win_open()
     Serial.println(" ");
     servo1.attach(servo_pin);
     servo1.write(72);
-    delay(1200); 
+    delay(1000); 
     servo1.detach();
-
 }
   
   void loop()
@@ -129,6 +143,8 @@ void win_open()
     rain = analogRead(water);
     h = dht.readHumidity();
     t = dht.readTemperature();
+    str = t;
+  
     Serial.print(F("Humidity: "));
     Serial.print(h);
     Serial.print(F("%  Temperature: "));
@@ -143,6 +159,7 @@ void win_open()
     Serial.println(" ");
     Serial.print("WATER LEVEL ");
     Serial.print(rain);
+  
     Serial.println(" ");
     if (button1 == 1)
     {
@@ -152,7 +169,11 @@ void win_open()
   
     if (but_flag == 0 && button1 == 1)
     {
-      win_close();
+      boolean is_closed =  win_close();
+      if (is_closed == false)
+      {
+        warning();
+      }
     }
     else if ((but_flag == 1 && button1 == 1))
     { //butflag == 1  open window 
@@ -169,83 +190,58 @@ void win_open()
     {
       alarm();
     }
- WiFiClient client = server.available();   // Listen for incoming clients
+    unsigned long currentMillis = millis();
+    WiFiClient client = server.available();   // Listen for incoming clients
+    if (WiFi.softAPgetStationNum() > 0 && currentMillis - previousTime > timeoutTime) 
+    {
+      previousTime = currentMillis;
+      Serial.println("User connected to ESP32's local WiFi");
+      HTTPClient client;
+      String url = "http://192.168.4.2:81/api/window";
+      client.begin(url.c_str());
+      Serial.println("HTTP CODE GET: ");
+      int httpCode2 = client.GET();
+      Serial.println(httpCode2);
+      client.end();
+      if (httpCode2 > 0) 
+      {
+        client.begin(url.c_str());
+        client.addHeader("Content-Type", "application/json");
+        String temperatureStr = "\"" + String(t) + "\"";
+        String humidityStr = "\"" + String(h) + "\"";
+        String isOpenStr = is_window_open() ? "true" : "false";
+        isOpenStr ="\"" + isOpenStr + "\"";
+        String waterLevel = "\"" + String(rain)+ "\"";
+        String is_protected = is_protected ? "true" : "false";
+        is_protected ="\"" + is_protected + "\"";
 
-  if (client) {                             // If a new client connects,
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {
-      // loop while the client's connected
-      currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /16/on") >= 0) {
-              win_open();
-          // turns the LED on
-            } else if (header.indexOf("GET /16/off") >= 0) {
-              win_close();
-            }
-            
-          
-
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons
-            client.println("<style>html { font-family: monospace; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: yellowgreen; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 32px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: gray;}</style></head>");
-
-            client.println("<body><h1>ESP32 Web Server</h1>");
-            client.println("<p>Control LED State</p>");
-
-            if (is_window_open() == false) {
-              client.println("<p><a href=\"/16/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/16/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-            
-
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        String jsonString = "{\"temperature\": " +  temperatureStr + ", \"humidity\": " + humidityStr + ", \"isOpen\": " + isOpenStr + "}";
+        Serial.println("JSON STRING: ");
+        Serial.println(jsonString);
+        int httpResponseCode = client.POST(jsonString);
+        Serial.println("HTTP POST  CODE: ");
+        Serial.println(httpResponseCode);
+        if (httpResponseCode > 0) {
+          String response = client.getString();
+          Serial.println("Response:");
+          Serial.println(response);
+        } 
+        else 
+        {
+          Serial.println("Error: No response received");
         }
+        client.end();
       }
+    delay(500);
     }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
+    delay(500);
   }
 
-  }
+class MyClass {
+public:
+    // Constructor to initialize the attribute
+     String test;
+    };
 
   
 

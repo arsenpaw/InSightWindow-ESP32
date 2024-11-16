fa842da550acf274bf6beda75bc11d0cdf0284d4
Arsen Pavliuk <123877570+arsenpaw@users.noreply.github.com>
2024-11-16 17:11:35 +0200

    feat: add servo handle

---
+++ IoT_App/IoT_App/Motor/IStepMotorService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Text;
+
+namespace IoT_App.Motor
+{
+    public interface IStepMotorService
+    {
+        public void Rotate(int degrees);
+    }
+}
---
+++ IoT_App/IoT_App/Motor/StepMotor.cs
@@ -0,0 +1,26 @@
+﻿using Iot.Device.Uln2003;
+using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace IoT_App.Motor
+{
+    public class StepMotor: IStepMotorService
+    {
+        private Uln2003 Motor { get; set; }
+        public StepMotor(int pin1,int pin2, int pin3, int pin4)
+        {
+            Motor = new Uln2003 (pin1, pin2, pin3, pin4);
+            Motor.Mode = StepperMode.FullStepDualPhase;
+            Motor.RPM = short.MaxValue;
+        }
+
+        public void Rotate(int degrees)
+        {
+            Debug.WriteLine($"Rotating {degrees} degrees");        
+             Motor.Step(degrees); 
+
+        }
+    }
+    
+}
